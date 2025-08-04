8b32453757ac8e837ad0c53f64a28f343c5d55bd
srinatasjya <srinatasjya@gmail.com>
2025-08-04 16:18:38 +0700

    Add files via upload

---
+++ 20250804-161803.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"aplikasi penjumlahan";
+    int bil1,bil2,hasil;
+    cout << "silahkan masukan bil1" << endl;
+    cin>>bil1;
+    cout << "silahkan masukan bil2" << endl;
+    cin>> bil2;
+    hasil=bil1+bil2;
+    cout <<hasil;
+    
+    
+    
+    
+    
+     
+    return 0;
+}
