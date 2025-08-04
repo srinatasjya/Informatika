73a01e536abff80a3612b92d8f3ea52996404e24
srinatasjya <srinatasjya@gmail.com>
2025-08-04 12:48:30 +0700

    Add files via upload

---
+++ 20250804-124550.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Nama   :Natasjya Sri lestari" << endl;
+    cout << "kelas. : x IPS" << endl;
+    cout << "sekolah:MA Almanshuriyah" << endl;
+    cout << "umur   :15" << endl;
+    cout << "TTL    :cianjur 05022010" << endl;
+    
+    
+    
+    
+     
+    return 0;
+}
